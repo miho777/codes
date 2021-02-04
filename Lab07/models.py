918e968075660207930d74daf051d5c456f3daad
miho777 <naozumi.miho@emc.com>
2021-02-04 11:15:45 +0900

    from #2 VSC

--- Lab07/models.py
+++ Lab07/models.py
@@ -24,8 +24,8 @@ from config import ecs_test_drive
 
 # Otherwise, assume running locally with local MongoDB instance
 #else:
-client = MongoClient("mongodb://USERNAME:PASSWORD@cluster0-shard-00-00.mzmpq.mongodb.net:27017,cluster0-shard-00-01.mzmpq.mongodb.net:27017,cluster0-shard-00-02.mzmpq.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qm2w8-shard-0&authSource=admin&retryWrites=true&w=majority")
-DB_NAME = "testdb1"  ##### Make sure this create your unique MongoDB database name######
+client = MongoClient("mongodb://piperdb1:GeOB87OlaR1Y6rkt@cluster0-shard-00-00.mzmpq.mongodb.net:27017,cluster0-shard-00-01.mzmpq.mongodb.net:27017,cluster0-shard-00-02.mzmpq.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qm2w8-shard-0&authSource=admin&retryWrites=true&w=majority")
+DB_NAME = "testdb777m"  ##### Make sure this create your unique MongoDB database name######
 
 # Get database connection with database name
 db = client[DB_NAME]
